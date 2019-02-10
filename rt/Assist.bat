59be3f3d0c7b83080c7614900bcef76f295dec1b
z390-dev <z390-dev@t-online.de>
2019-02-10 17:46:03 +0100

    Added ASSIST - batches, RT, and saved

---
+++ rt/Assist.bat
@@ -0,0 +1,53 @@
+@ECHO OFF
+@SETLOCAL
+
+@SET dirWork=assist
+@SET dirRtWork=.\rt\%dirWork%
+@SET dirRtSaved=%dirRtWork%\save
+@SET dirRtDiffs=%dirRtWork%\diff
+@SET sysTerm=systerm(%dirWork%\RTASSIST)
+
+@ECHO.
+@ECHO RT Assist - Run %dirWork% programs in ..\%dirWork% and generate %dirRtDiffs%
+
+@ECHO.
+@ECHO RT Assist - Go one level up to z390 main program directory
+@PUSHD ..
+
+@ECHO.
+@ECHO RT Assist - Clearing all working files in working directory %dirWork%
+@CALL .\rt\ClearDir.bat %dirWork%
+
+@ECHO.
+@ECHO RT Assist - Running %dirWork% programs...
+@CALL ASSIST %dirWork%\DEMOAST1 notiming stats %sysTerm% %2 %3 %4 %5 %6 %7 %8 %9
+@CALL ASSIST %dirWork%\TESTAST1 notiming stats %sysTerm% %2 %3 %4 %5 %6 %7 %8 %9
+@CALL ASSIST %dirWork%\SOLP06   notiming stats %sysTerm% %2 %3 %4 %5 %6 %7 %8 %9
+
+@ECHO.
+@ECHO RT Assist - Creating rt-diff and rt-save directories (if necessary).
+@IF NOT EXIST %dirRtWork%\nul   @MKDIR %dirRtWork%
+@IF NOT EXIST %dirRtDiffs%\nul  @MKDIR %dirRtDiffs%
+@IF NOT EXIST %dirRtSaved%\nul  @MKDIR %dirRtSaved%
+
+@ECHO.
+@ECHO RT Assist - Erasing previous diff files in %dirRtDiffs%
+@ERASE /Q %dirRtDiffs%\*.*
+
+:doCompare
+@ECHO.
+@ECHO RT Assist - Compare %dirWork% files with %dirRtSaved% files and generate %dirRtDiffs% files
+@ECHO.
+@java -classpath z390.jar -Xrs %J2SEOPTIONS% DiffRegrTests %dirWork% %dirRtSaved% %dirRtDiffs%
+
+:doListing
+@ECHO.
+@ECHO RT Assist - Displaying ordered list of %dirRtDiffs%
+@DIR /OS /P %dirRtDiffs%
+
+@ECHO.
+@ECHO RT Assist - Going back to rt working directory
+@POPD
+@ENDLOCAL
+
+@ECHO RT Assist - Finished!
---
+++ rt/SaveAssist.bat
@@ -0,0 +1,43 @@
+@ECHO OFF
+@SETLOCAL
+
+@SET dirWork=assist
+@SET dirRtWork=.\rt\%dirWork%
+@SET dirRtSaved=%dirRtWork%\save
+@SET dirRtDiffs=%dirRtWork%\diff
+
+@ECHO.
+@ECHO RT Save Assist - Clean %dirRtDiffs% and save rt assist files to %dirRtSaved%
+
+@ECHO.
+@ECHO RT Save Assist - Go one level up to z390 main program directory
+@PUSHD ..
+
+@ECHO.
+@ECHO RT Save Assist - Creating rt diff and rt save directories (if necessary).
+@IF NOT EXIST %dirRtWork%\nul   @MKDIR %dirRtWork%
+@IF NOT EXIST %dirRtDiffs%\nul  @MKDIR %dirRtDiffs%
+@IF NOT EXIST %dirRtSaved%\nul  @MKDIR %dirRtSaved%
+
+@ECHO.
+@ECHO RT Save Assist - Erasing previous rt-diff files in %dirRtDiffs%
+@ERASE /Q %dirRtDiffs%\*.*
+
+@ECHO.
+@ECHO RT Save Assist - Erasing previous rt-save files in %dirRtSaved%
+@ERASE /Q %dirRtSaved%\*.*
+
+@ECHO.
+@ECHO RT Save Assist - Copying working files to rt-save in %dirRtSaved%
+@COPY %dirWork%\*.* %dirRtSaved%
+
+@ECHO.
+@ECHO RT Save Assist - Cleaing working directory %dirWork%
+@CALL .\rt\ClearDir.bat %dirWork%
+
+@ECHO.
+@ECHO RT Save Assist - Going back to rt working directory
+@POPD
+@ENDLOCAL
+
+@ECHO RT Save Assist - Finished!
